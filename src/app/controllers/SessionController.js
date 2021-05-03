ed43120621d36bc6f3eeccc8f817b8f9ee0f05e6
Emanoel Johannes <emanoeljohannes@hotmail.com>
2021-05-03 13:37:31 -0300

    Tirando CPF

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -7,41 +7,26 @@ class SessionController {
     async store(req, res){
         var { login, senha } = req.body;
 
-        if (!isNaN(login)){
-            const cpf = await User.findByCPF(login);
-            
-            if (cpf.length > 0) {
-                const user = await User.AutenticateByCPF(cpf[0].cpf, senha);
+        var loginNoWhiteSpace = login.replace(/\s/g, "");
 
-                if (user.length < 1) {
-                    return res.json({ error: "Senha incorreta." });
-                } else {                    
-                    return res.json({user})
-                }
-            } else {
-                return res.json({error: "CPF não encontrado."});
-            }
+        const checkUser = await User.findByLogin(loginNoWhiteSpace);
+
+        if (checkUser.length < 1) {
+        
+            return res.json({ error: "Login não encontrado." });
+        
         } else {
-            var loginNoWhiteSpace = login.replace(/\s/g, "");
 
-            const checkUser = await User.findByLogin(loginNoWhiteSpace);
+            const user = await User.Autenticate(loginNoWhiteSpace, senha);
 
-            if (checkUser.length < 1) {
-            
-                return res.json({ error: "Login não encontrado." });
-            
-            } else {
-
-                const user = await User.Autenticate(loginNoWhiteSpace, senha);
-
-                if (user.length < 1) {
-                    return res.json({ error: "Senha incorreta." });
-                } else {                    
-                    return res.json({user})
-                }  
-            }
-        }              
-    }
+            if (user.length < 1) {
+                return res.json({ error: "Senha incorreta." });
+            } else {                    
+                return res.json({user})
+            }  
+        }
+    }              
+    
 }
 
 module.exports = new SessionController();
